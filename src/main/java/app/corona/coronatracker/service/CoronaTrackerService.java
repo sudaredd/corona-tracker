cca789ce978543ecb9681d6b9cec469197e93db4
sudaredd <sudaredd@gmail.com>
2020-02-29 12:03:11 -0500

    Add Non blocked cache

--- src/main/java/app/corona/coronatracker/service/CoronaTrackerService.java
+++ src/main/java/app/corona/coronatracker/service/CoronaTrackerService.java
@@ -16,6 +16,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -25,10 +26,11 @@ public class CoronaTrackerService {
 
     public static final String CORONA_STATUS_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv";
 
-    private List<CoronaStats> coronaStatsList = new ArrayList<>();
+    private AtomicReference<List<CoronaStats>> cachedReference = new AtomicReference<>();
 
     @PostConstruct
-    @Scheduled(cron = "0 45 6 ? * *")
+//    @Scheduled(cron = "0 45 6 ? * *")
+    @Scheduled(cron = "0/30 * * ? * * ")
     public void init() throws IOException, InterruptedException {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest httpRequest = HttpRequest.newBuilder(URI.create(CORONA_STATUS_URL))
@@ -38,12 +40,12 @@ public class CoronaTrackerService {
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(stringReader);
         List<CoronaStats> list = StreamSupport.stream(records.spliterator(), false)
                 .map(this::coronaStat).collect(Collectors.toList());
-        coronaStatsList = list;
+        cachedReference.compareAndSet(cachedReference.get(), list);
         log.info("records:" + list);
     }
 
    public List<CoronaStats> latestCoronaStats() {
-        return coronaStatsList;
+        return cachedReference.get();
     }
 
     private CoronaStats coronaStat(CSVRecord record) {
